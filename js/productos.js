c96fcad1bbdf60412539cb41b3e452ec78b76a0d
GiulianaCravotta <giulianacravotta@gmail.com>
2023-10-01 06:18:31 -0300

    Update productos.json data source

--- js/productos.js
+++ js/productos.js
@@ -14,7 +14,7 @@ let listaCarritoItems = [];
 let data;
 
 //Carga de datos del JSON
-fetch("js/productos.json")
+fetch("https://raw.githubusercontent.com/GiulianaCravotta/ProyectoFinalCravotta-JS/master/js/productos.json")
     .then(response => response.json())
     .then(dataResponse => {
         data = dataResponse;
